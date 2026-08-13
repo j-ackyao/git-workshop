b9b1ba8fad71b866eb0b7a5cd913801cb99e15b8
j-ackyao <jackyao2004@gmail.com>
2026-08-13 12:49:47 -0700

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,11 @@
+from builder import build
+
+def main():
+    """
+    Main entry to call build()
+    - Sincerely, from your colleague
+    """
+    build()
+
+if __name__ == "__main__":
+    main()
